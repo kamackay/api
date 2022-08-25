5859ad1cf56344f49ded18d71fb331f01b8be6e9
Keith MacKay <keith@keithmackay.com>
2022-08-25 15:09:20 -0400

    Formatting

--- src/main/java/com/keithmackay/api/services/AdBlockService.kt
+++ src/main/java/com/keithmackay/api/services/AdBlockService.kt
@@ -19,91 +19,91 @@ const val pageSize = 2000
 @Singleton
 class AdBlockService @Inject
 internal constructor(
-        db: Database,
-        ephemeralDatabase: EphemeralDatabase
+    db: Database,
+    ephemeralDatabase: EphemeralDatabase
 ) {
-    private val log = getLogger(this::class)
+  private val log = getLogger(this::class)
 
-    private val remoteLsCollection = db.getCollection("lsrules")
-    private val localLsCollection = ephemeralDatabase.getCollection("lsrules")
-    private val rulesCache = Cacher<Set<String>>(Duration.ofMinutes(15), "LS Block Hosts")
+  private val remoteLsCollection = db.getCollection("lsrules")
+  private val localLsCollection = ephemeralDatabase.getCollection("lsrules")
+  private val rulesCache = Cacher<Set<String>>(Duration.ofMinutes(15), "LS Block Hosts")
 
-    fun getBlockedServers(): Set<String> {
-        return rulesCache.get("all") {
-            fastest(supplyAsync {
-                remoteLsCollection.find()
-                        .projection(doc("server", 1))
-                        .into(ArrayList())
-                        .map { it.getString("server") }
-                        .toSet()
-            }, supplyAsync {
-                localLsCollection.find()
-                        .projection(doc("server", 1))
-                        .into(ArrayList())
-                        .map { it.getString("server") }
-                        .toSet()
-            }).join()
-        }
+  fun getBlockedServers(): Set<String> {
+    return rulesCache.get("all") {
+      fastest(supplyAsync {
+        remoteLsCollection.find()
+            .projection(doc("server", 1))
+            .into(ArrayList())
+            .map { it.getString("server") }
+            .toSet()
+      }, supplyAsync {
+        localLsCollection.find()
+            .projection(doc("server", 1))
+            .into(ArrayList())
+            .map { it.getString("server") }
+            .toSet()
+      }).join()
     }
+  }
 
-    fun isBlocked(domain: String): Boolean {
-        val lowerDomain = domain.lowercase()
-        return getBlockedServers().stream()
-                .filter(Objects::nonNull)
-                .filter(Strings::isNotEmpty)
-                .map(String::lowercase)
-                .anyMatch(lowerDomain::equals)
+  fun isBlocked(domain: String): Boolean {
+    val lowerDomain = domain.lowercase()
+    return getBlockedServers().stream()
+        .filter(Objects::nonNull)
+        .filter(Strings::isNotEmpty)
+        .map(String::lowercase)
+        .anyMatch(lowerDomain::equals)
+  }
+
+  fun doCacheSync(trigger: String = "manual", chunkSize: Int = pageSize): CacheSyncResult {
+    log.info("Running cache sync. Trigger: $trigger")
+    stepCarefully(listOf {
+      localLsCollection.createIndex(
+          doc("server", 1),
+          IndexOptions().unique(true)
+      )
+      Unit
+    }) {
+      log.info("Index Already Exists")
     }
-
-    fun doCacheSync(trigger: String = "manual", chunkSize: Int = pageSize): CacheSyncResult {
-        log.info("Running cache sync. Trigger: $trigger")
-        stepCarefully(listOf {
-            localLsCollection.createIndex(
-                    doc("server", 1),
-                    IndexOptions().unique(true)
-            )
-            Unit
-        }) {
-            log.info("Index Already Exists")
-        }
-        iterateRemoteServers().iterate {
-            try {
-                localLsCollection.insertOne(it)
-            } catch (e: MongoWriteException) {
-                // already in the database
-                return@iterate
-            } catch (e: Exception) {
-                log.warn("Couldn't insert", e)
-            }
-        }
-        val local = localLsCollection.countDocuments()
-        val remote = remoteLsCollection.countDocuments()
-        log.info("Local: $local - Remote $remote${if (remote == local) "" else "!!!!!"}")
-        return CacheSyncResult(local, remote)
+    iterateRemoteServers().iterate {
+      try {
+        localLsCollection.insertOne(it)
+      } catch (e: MongoWriteException) {
+        // already in the database
+        return@iterate
+      } catch (e: Exception) {
+        log.warn("Couldn't insert", e)
+      }
     }
+    val local = localLsCollection.countDocuments()
+    val remote = remoteLsCollection.countDocuments()
+    log.info("Local: $local - Remote $remote${if (remote == local) "" else "!!!!!"}")
+    return CacheSyncResult(local, remote)
+  }
 
-    data class CacheSyncResult(val local: Long, val remote: Long)
+  data class CacheSyncResult(val local: Long, val remote: Long)
 
-    private fun iterateRemoteServers(chunkSize: Int = pageSize): Channel<Document> {
-        val channel = Channel<Document>()
-        async {
-            val count = remoteLsCollection.countDocuments(doc())
-            var transferred = 0
-            var page = 0
-            while (transferred < count) {
-                log.info("Transferring Documents in page $page/${count / chunkSize}")
-                val documents = remoteLsCollection.find(doc())
-                        .limit(chunkSize)
-                        .skip(page++ * chunkSize)
-                        .into(ArrayList())
-                documents.forEach {
-                    log.debug("Pushing document to channel")
-                    channel.push(it)
-                }
-                transferred += documents.size
-            }
-            channel.close()
+  private fun iterateRemoteServers(chunkSize: Int = pageSize): Channel<Document> {
+    val channel = Channel<Document>()
+    async {
+      val count = remoteLsCollection.countDocuments(doc())
+      var transferred = 0
+      var page = 0
+      while (transferred < count) {
+        log.info("Transferring Documents in page $page/${count / chunkSize}")
+        val documents = remoteLsCollection.find(doc())
+            .limit(chunkSize)
+            .skip(page++ * chunkSize)
+            .into(ArrayList())
+        documents.forEach {
+          log.debug("Pushing document to channel")
+          channel.push(it)
         }
-        return channel
+        transferred += documents.size
+      }
+      channel.close()
     }
+    return channel
+  }
 }
